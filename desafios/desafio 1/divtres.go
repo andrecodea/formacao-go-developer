51e33fd1af12e41b2da9e82f269bf8266fce3e24
André Codea <61255315+andrecodea@users.noreply.github.com>
2025-06-15 18:07:39 -0300

    Update divtres.go

--- desafios/desafio 1/divtres.go
+++ desafios/desafio 1/divtres.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 )
 
+// Cria função que detecta números divisíveis por 3
 func divtres(limite int) {
+	// Para cada i que começa em 1 e tem o limite definido pelo usuário...
 	for i := 1; i <= limite; i++ {
+		// Se i for divisível por 3...
 		if i%3 == 0 {
+			// Imprima a mensagem
 			fmt.Printf("O valor %v é divisível por 3\n", i)
 
 		} else {
@@ -15,6 +19,7 @@ func divtres(limite int) {
 	}
 }
 
+// Chama a função
 func main() {
 	divtres(100)
 }
